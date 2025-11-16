ed41b4fc7d68c71247cbcef5c0697f56f90c925f
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-11-16 15:07:03 -0700

    added backend-to-UI redirect
    A GET handler on the / route

--- backend/handlers/handleRoot.go
+++ backend/handlers/handleRoot.go
@@ -1,9 +1,7 @@
 package handlers
 
 import (
-	"encoding/json"
 	"fmt"
-	"myfriends-backend/models"
 	"net/http"
 	"time"
 )
@@ -11,20 +9,11 @@ import (
 func Root(w http.ResponseWriter, req *http.Request) {
 
 	// Log request type
-	fmt.Printf("\n%v: %v\n%v\n\n", time.Now(), req.Method, req.Body)
+	fmt.Printf("%v: %v %v\n", time.Now().Format(time.DateTime), req.Method, req.RequestURI)
 
-	if req.Method == http.MethodPost {
-
-		var formData models.Friend
-		err := json.NewDecoder(req.Body).Decode(&formData)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Form submitted successfully."))
-		return
-
+	if req.Method == http.MethodGet {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte(`Welcome to the headless backend. If you'd like to use a UI, check out the frontend at <a href="http://localhost:5173/">http://localhost:5173/</a>.`))
 	}
 
 }
