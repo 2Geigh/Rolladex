d88b8ed3af9eff2f605c7822bfd24c1499d510f3
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-11-22 20:09:06 -0700

    scaffolded login form

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,7 +1,25 @@
+import type { FormEventHandler } from "react"
+import "./dist/Login.min.css"
+
 const Login = () => {
+
+    // const onSubmit: FormEventHandler = (/*e: React.FormEvent<Element>*/) => {
+        // alert("Form submitted!")
+    // }
+
     return (
         <>
-            This is the Login Component.
+            <form id="loginForm" method="POST" action="/login">
+                <label htmlFor="username">
+                    Username <input required name="username" type="text"></input>
+                </label>
+                
+                <label htmlFor="password">
+                    Password <input required name="password" type="password"></input>
+                </label> 
+                
+                <input id="loginButton" type="submit" value="Login"></input>
+            </form>
         </>
     )
 }
