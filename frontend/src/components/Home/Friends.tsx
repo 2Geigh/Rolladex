5a22f6aeea1353fd3e16255eb32bb3f0fe62768a
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-11-25 14:27:31 -0700

    made friendslist items responsive to NaN values
    They now won't show a specific "days since" section if there isn't a previous date listed for their last meetup or last interaction

--- frontend/src/components/Home/Friends.tsx
+++ frontend/src/components/Home/Friends.tsx
@@ -1,62 +1,65 @@
 import "./styles/dist/Friends.min.css"
 import { daysSinceDate } from "../../util/dates"
-import type { Friend } from "../../types/Friend"
+import type { Friend } from "../../types/friend";
 import friends from "../../util/friends_sample_data"
 
 export default function Friends() {
 
     // Render friends list into UI elements 
     const friendElements = friends.map((item: Friend, index: number) => {
-        return(
+        const lastInteractionDays = daysSinceDate(String(item.last_interaction));
+        const lastMeetupDays = daysSinceDate(String(item.last_meetup));
 
+        return (
             <li className="friendListItem" id={String(index)} key={index}>
                 <div className="nameAndPhoto">
                     <a href={`/friends/${String(item.id)}`} className="name">{String(item.name)}</a>
-                    <a href={`/friends/${String(item.id)}`}><img src={String(item.profile_image)} alt={String(item.name)} className="profile_photo" /></a>
+                    <a href={`/friends/${String(item.id)}`}>
+                        <img src={String(item.profile_image)} alt={String(item.name)} className="profile_photo" />
+                    </a>
                 </div>
 
-                <div className="previousTimes">
-                    <span className="lastTime" id="friendLastInteraction">
-                        <span className="label">Last interaction:</span>
-                        <span className="daysAgo">
-                            <span className="numberOfDays">
-                                {daysSinceDate(String(item.last_interaction))}
+                { (!isNaN(lastInteractionDays) || !isNaN(lastMeetupDays)) && (
+                    <div className="previousTimes">
+                        { !isNaN(lastInteractionDays) && (
+                            <span className="lastTime" id="friendLastInteraction">
+                                <span className="label">Last interaction:</span>
+                                <span className="daysAgo">
+                                    <span className="numberOfDays">{lastInteractionDays}</span><br />
+                                    days ago
+                                </span>
                             </span>
-                            <br></br>
-                            days ago
-                        </span>
-                    </span>
+                        )}
 
-                    <span className="lastTime" id="friendLastMeetup">
-                        <span className="label">Last meetup:</span>
-                        <span className="daysAgo">
-                            <span className="numberOfDays">
-                                {daysSinceDate(String(item.last_meetup))}
+                        { !isNaN(lastMeetupDays) && (
+                            <span className="lastTime" id="friendLastMeetup">
+                                <span className="label">Last meetup:</span>
+                                <span className="daysAgo">
+                                    <span className="numberOfDays">{lastMeetupDays}</span><br />
+                                    days ago
+                                </span>
                             </span>
-                            <br></br>
-                            days ago
-                        </span>
-                    </span>
-                </div>
+                        )}
+                    </div>
+                )}
+                
 
                 <div className="deleteAndOptionalAlert">
                     <div className="alert">No alerts yet.</div>
                     <button className="deleteFriend">Delete friend</button>
                 </div>
-
             </li>
-
-    )})
+        );
+    });
 
     return (
         <>
             <div id="friends" className="homepage_segment">
                 <h2>Friends:</h2>
-
                 <ul className="friendList">
                     {friendElements}
                 </ul>
             </div>
         </>
-    )
+    );
 }
