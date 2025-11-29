9687ab580a0f8705855c4b8931efc2a437c8d14e
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-11-29 12:53:43 -0700

    modulated <Friends/> component into sub-component family

--- frontend/src/components/Home/Friends.tsx
+++ frontend/src/components/Home/Friends.tsx
@@ -3,6 +3,65 @@ import { daysSinceDate } from "../../util/dates"
 import type { Friend } from "../../types/friend";
 import friends from "../../util/friends_sample_data"
 
+const NameAndPhoto: React.FC<{ ID: Friend["id"], name: Friend["name"], profile_image_path: Friend["profile_image_path"]}> = ( { ID, name, profile_image_path } ) => {
+
+    const profilePath = `/friends/${String(ID)}`;
+
+    return (
+        <div className="nameAndPhoto">
+
+            <a href={profilePath} className="name">{String(name)}</a>
+
+            <a href={profilePath}>
+                <img src={String(profile_image_path)} alt={String(name)} className="profile_photo" />
+            </a>
+
+        </div>
+    )
+}
+
+const DaysSinceLastInteraction: React.FC<{ lastInteractionDays: number}> = ( { lastInteractionDays } ) => {
+    return (
+        <>
+                        <span className="lastTime" id="friendLastInteraction">
+                            <span className="label">Last interaction:</span>
+                            <span className="daysAgo">
+                                { !isNaN(lastInteractionDays) && (
+                                    <span className="numberOfDays">{lastInteractionDays}</span>
+                                )}
+                                { isNaN(lastInteractionDays) && (
+                                    <span className="numberOfDays">?</span>
+                                )}
+                                <br />
+                                days ago
+                            </span>
+                        </span>
+        </>
+    )
+}
+
+const DaysSinceLastMeetup: React.FC<{lastMeetupDays: number}> = ({ lastMeetupDays }) => {
+    return (
+        <>
+                        <span className="lastTime" id="friendLastMeetup">
+                            <span className="label">Last meetup:</span>
+                            
+                            <span className="daysAgo">
+                                { !isNaN(lastMeetupDays) && (
+                                    <span className="numberOfDays">{lastMeetupDays}</span>
+                                )}
+                                { isNaN(lastMeetupDays) && (
+                                    <span className="numberOfDays">?</span>
+                                )}
+                                <br />
+                                days ago
+                            </span>
+                        </span>
+
+        </>
+    )
+}
+
 export default function Friends() {
 
     // Render friends list into UI elements 
@@ -12,34 +71,16 @@ export default function Friends() {
 
         return (
             <li className="friendListItem" id={String(index)} key={index}>
-                <div className="nameAndPhoto">
-                    <a href={`/friends/${String(item.id)}`} className="name">{String(item.name)}</a>
-                    <a href={`/friends/${String(item.id)}`}>
-                        <img src={String(item.profile_image_path)} alt={String(item.name)} className="profile_photo" />
-                    </a>
-                </div>
+                
+                <NameAndPhoto ID={item.id} name={item.name} profile_image_path={item.profile_image_path}/>
 
                 { (!isNaN(lastInteractionDays) || !isNaN(lastMeetupDays)) && (
                     <div className="previousTimes">
-                        { !isNaN(lastInteractionDays) && (
-                            <span className="lastTime" id="friendLastInteraction">
-                                <span className="label">Last interaction:</span>
-                                <span className="daysAgo">
-                                    <span className="numberOfDays">{lastInteractionDays}</span><br />
-                                    days ago
-                                </span>
-                            </span>
-                        )}
 
-                        { !isNaN(lastMeetupDays) && (
-                            <span className="lastTime" id="friendLastMeetup">
-                                <span className="label">Last meetup:</span>
-                                <span className="daysAgo">
-                                    <span className="numberOfDays">{lastMeetupDays}</span><br />
-                                    days ago
-                                </span>
-                            </span>
-                        )}
+                        <DaysSinceLastInteraction lastInteractionDays={lastMeetupDays} />
+
+                        <DaysSinceLastMeetup lastMeetupDays={lastMeetupDays}/>
+
                     </div>
                 )}
                 
@@ -62,4 +103,5 @@ export default function Friends() {
             </div>
         </>
     );
+    
 }
