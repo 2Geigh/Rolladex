243bcd5338850e855f1acdf32251cec062d8efb0
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-12-13 12:16:49 -0700

    moved signup methods to handleSignup.go

--- backend/handlers/handleSignup.go
+++ backend/handlers/handleSignup.go
@@ -1,11 +1,25 @@
 package handlers
 
 import (
+	"crypto/rand"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
+	"math/big"
+	"myfriends-backend/database"
 	"myfriends-backend/models"
 	"myfriends-backend/util"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	// Define pepper ASCII range
+	PepperAsciiMax   = 126 // ~
+	PepperAsciiMin   = 34  // "
+	PepperAsciiRange = new(big.Int).SetInt64(int64(PepperAsciiMax - PepperAsciiMin))
 )
 
 func Signup(w http.ResponseWriter, req *http.Request) {
@@ -46,7 +60,7 @@ func Signup(w http.ResponseWriter, req *http.Request) {
 		}
 
 		// Create new user
-		err = models.RegisterUser(user)
+		err = RegisterUser(user)
 		if err != nil {
 			util.ReportHttpError(err, w, "couldn't insert user into database", http.StatusInternalServerError)
 			return
@@ -54,4 +68,114 @@ func Signup(w http.ResponseWriter, req *http.Request) {
 
 		w.WriteHeader(http.StatusOK)
 	}
+}
+
+func RegisterUser(user models.User) error {
+	var (
+		err        error
+		dbFilePath string = "database/myFriends.db"
+	)
+
+	// Connect to database
+	DB, err := database.InitializeDB(dbFilePath)
+	if err != nil {
+		return fmt.Errorf("couldn't initialize database: %w", err)
+	}
+	defer DB.Close()
+
+	// Check if user already exists in database
+	userExists, err := UserExists(DB, user.Username)
+	if err != nil {
+		return fmt.Errorf("failed to check if user already exists in database: %w", err)
+	}
+	if userExists {
+		return fmt.Errorf("user already exists in database")
+	}
+
+	// Prepare password for database storage
+	hashedPassword, err := HashPassword(user.Password)
+	if err != nil {
+		return fmt.Errorf("failed to prepare password for database storage: %w", err)
+	}
+
+	// Add user to database
+	stmt, err := DB.Prepare("INSERT INTO Users (username, password) VALUES (?, ?)")
+	if err != nil {
+		return fmt.Errorf("failed to add user to database: %w", err)
+	}
+	result, err := stmt.Exec(user.Username, hashedPassword)
+	if err != nil {
+		return fmt.Errorf("failed to add user to database: %v", err)
+	}
+	stmt.Close()
+
+	// View SQL statement results
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	util.LogWithTimestamp(fmt.Sprintf("Registered \033[3m%v\033[0m as a user (Affected %d rows)", user.Username, rowsAffected))
+	return err
+}
+
+func UserExists(DB *sql.DB, username string) (bool, error) {
+
+	var (
+		count int
+		err   error
+	)
+
+	// Prepare SQL statement
+	stmt, err := DB.Prepare("SELECT COUNT(*) FROM Users WHERE Username = ?")
+	if err != nil {
+		return false, fmt.Errorf("failed to prepare SQL statement: %v", err)
+	}
+	defer stmt.Close()
+
+	// Execute the query
+	err = stmt.QueryRow(username).Scan(&count)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// No user found, return false
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to execute query: %v", err)
+	}
+
+	// Check if count is greater than 0
+	return count > 0, nil
+}
+
+func HashPassword(password string) (string, error) {
+	var (
+		err        error
+		costFactor int = bcrypt.DefaultCost
+	)
+
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), costFactor)
+	if err != nil {
+		err = fmt.Errorf("could not hash password: %w", err)
+	}
+
+	return string(hashedBytes), err
+}
+
+func PepperPassword(password string) (string, error) {
+
+	// Generate random pepper
+	pepperBigInt, err := rand.Int(rand.Reader, PepperAsciiRange)
+	if err != nil {
+		return "", err
+	}
+
+	// Convert random pepper *big.Int into a rune
+	pepperInt32 := int32(pepperBigInt.Int64())
+	pepperRune := rune(pepperInt32)
+
+	// Pepper the password
+	pepperedPassword := password + string(pepperRune)
+
+	return pepperedPassword, err
+
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,23 +1,9 @@
 package models
 
 import (
-	"crypto/rand"
-	"database/sql"
-	"fmt"
-	"math/big"
-	"myfriends-backend/database"
-	"myfriends-backend/util"
 	"time"
 
-	"golang.org/x/crypto/bcrypt"
 	_ "modernc.org/sqlite"
-)
-
-var (
-	// Define pepper ASCII range
-	PepperAsciiMax   = 126 // ~
-	PepperAsciiMin   = 34  // "
-	PepperAsciiRange = new(big.Int).SetInt64(int64(PepperAsciiMax - PepperAsciiMin))
 )
 
 type NotificationPreference struct {
@@ -74,114 +60,4 @@ type Session struct {
 	SessionToken string     `gorm:"not null;type:text"`
 	CreatedAt    time.Time  `gorm:"default:CURRENT_TIMESTAMP"`
 	ExpiresAt    *time.Time `gorm:"type:datetime"`
-}
-
-func RegisterUser(user User) error {
-	var (
-		err        error
-		dbFilePath string = "database/myFriends.db"
-	)
-
-	// Connect to database
-	DB, err := database.InitializeDB(dbFilePath)
-	if err != nil {
-		return fmt.Errorf("couldn't initialize database: %w", err)
-	}
-	defer DB.Close()
-
-	// Check if user already exists in database
-	userExists, err := UserExists(DB, user.Username)
-	if err != nil {
-		return fmt.Errorf("failed to check if user already exists in database: %w", err)
-	}
-	if userExists {
-		return fmt.Errorf("user already exists in database")
-	}
-
-	// Prepare password for database storage
-	hashedPassword, err := HashPassword(user.Password)
-	if err != nil {
-		return fmt.Errorf("failed to prepare password for database storage: %w", err)
-	}
-
-	// Add user to database
-	stmt, err := DB.Prepare("INSERT INTO Users (username, password) VALUES (?, ?)")
-	if err != nil {
-		return fmt.Errorf("failed to add user to database: %w", err)
-	}
-	result, err := stmt.Exec(user.Username, hashedPassword)
-	if err != nil {
-		return fmt.Errorf("failed to add user to database: %v", err)
-	}
-	stmt.Close()
-
-	// View SQL statement results
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	util.LogWithTimestamp(fmt.Sprintf("Registered \033[3m%v\033[0m as a user (Affected %d rows)", user.Username, rowsAffected))
-	return err
-}
-
-func UserExists(DB *sql.DB, username string) (bool, error) {
-
-	var (
-		count int
-		err   error
-	)
-
-	// Prepare SQL statement
-	stmt, err := DB.Prepare("SELECT COUNT(*) FROM Users WHERE Username = ?")
-	if err != nil {
-		return false, fmt.Errorf("failed to prepare SQL statement: %v", err)
-	}
-	defer stmt.Close()
-
-	// Execute the query
-	err = stmt.QueryRow(username).Scan(&count)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			// No user found, return false
-			return false, nil
-		}
-		return false, fmt.Errorf("failed to execute query: %v", err)
-	}
-
-	// Check if count is greater than 0
-	return count > 0, nil
-}
-
-func HashPassword(password string) (string, error) {
-	var (
-		err        error
-		costFactor int = bcrypt.DefaultCost
-	)
-
-	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), costFactor)
-	if err != nil {
-		err = fmt.Errorf("could not hash password: %w", err)
-	}
-
-	return string(hashedBytes), err
-}
-
-func PepperPassword(password string) (string, error) {
-
-	// Generate random pepper
-	pepperBigInt, err := rand.Int(rand.Reader, PepperAsciiRange)
-	if err != nil {
-		return "", err
-	}
-
-	// Convert random pepper *big.Int into a rune
-	pepperInt32 := int32(pepperBigInt.Int64())
-	pepperRune := rune(pepperInt32)
-
-	// Pepper the password
-	pepperedPassword := password + string(pepperRune)
-
-	return pepperedPassword, err
-
 }
