8c383293067c3a3ea94293e294778b31cb73f816
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-12-30 16:16:10 -0700

    signup page now redirects to login

--- frontend/src/components/Signup/Signup.tsx
+++ frontend/src/components/Signup/Signup.tsx
@@ -1,75 +1,89 @@
-import type { FormEvent, FormEventHandler } from "react"
+import { useState, type FormEvent } from "react"
 import Footer from "../Footer/Footer"
+import { Navigate } from "react-router-dom"
 import NavbarWithoutLinks from "../Navbar/NavbarWithoutLinks"
 import "./dist/Signup.min.css"
-import type { SignupData } from "../../util/login_signup_data"
-import { useNavigate } from "react-router"
+
+export type SignupData = {
+	username: string
+	password: string
+}
 
 const SignUp = () => {
+	const [hasSignedUp, setHasSignedUp] = useState(false)
 
-    const navigate = useNavigate()
-    // const onSubmit: FormEventHandler = (/*e: React.FormEvent<Element>*/) => {
-        // alert("Form submitted!")
-    // }
+	async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault()
 
-    const handleSubmit:FormEventHandler<HTMLFormElement> = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
+		const formData = new FormData(event.currentTarget as HTMLFormElement)
+		const data = Object.fromEntries(formData.entries())
 
+		const signupData: SignupData = {
+			username: String(data.username),
+			password: String(data.password),
+		}
 
-        const formData = new FormData(event.currentTarget as HTMLFormElement)
-        const data = Object.fromEntries(formData.entries());
-        
-        const signupData: SignupData = {
-            username:  String(data.username),
-            password: String(data.password)
+		const response = await fetch("http://localhost:3001/signup", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(signupData),
+		})
 
-        };
+		if (response.ok) {
+			alert(`Registered ${signupData.username} as a user`)
+			setHasSignedUp(true)
+		} else {
+			const errorText = await response.text()
+			alert(errorText)
+		}
+	}
 
-        const response = await fetch("http://localhost:3001/signup", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(signupData)
-        })
+	if (hasSignedUp) {
+		return <Navigate to="/login" />
+	}
 
-        if (response.ok) {
-            alert("Successfully created user")
-            navigate("/login")
-        } else {
-            const errorText = await response.text();
-            alert(errorText)
-        }
+	return (
+		<>
+			<NavbarWithoutLinks />
 
-    }
+			<div className="container">
+				<form id="signupForm" onSubmit={handleSubmit}>
+					<label htmlFor="username">
+						Username{" "}
+						<input required name="username" type="text"></input>
+					</label>
 
-    return (
-        <>
-            <NavbarWithoutLinks/>
-            
-            {/* <div className="content"> */}
-                <div className="container">
-                    <form id="signupForm" onSubmit={handleSubmit}>
-                        <label htmlFor="username">
-                            Username <input required name="username" type="text"></input>
-                        </label>
-                        
-                        <label htmlFor="password">
-                            Password <input required name="password" type="password"></input>
-                        </label> 
+					<label htmlFor="password">
+						Password{" "}
+						<input required name="password" type="password"></input>
+					</label>
 
-                        <div className="agreements">
-                            <input required type="checkbox" id="tos-agree" name="tos-agree"></input>
-                            <label htmlFor="tos-agree">I have read and agree to the <a href="terms_of_service">terms of service</a></label>
-                        
-                        <input id="loginButton" type="submit" value="Sign Up"></input>
-                    </form>
-                </div>
-            {/* </div> */}
+					<div className="agreements">
+						<input
+							required
+							type="checkbox"
+							id="tos-agree"
+							name="tos-agree"
+						></input>
+						<label htmlFor="tos-agree">
+							I have read and agree to the{" "}
+							<a href="terms_of_service">terms of service</a>
+						</label>
+					</div>
 
-            <Footer/>
-        </>
-    )
+					<input
+						id="loginButton"
+						type="submit"
+						value="Sign Up"
+					></input>
+				</form>
+			</div>
+
+			<Footer />
+		</>
+	)
 }
 
 export default SignUp
