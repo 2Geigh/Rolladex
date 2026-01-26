34a1914e50586f6d9748cdf69dd44c7c18d6911f
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-01-25 18:12:32 -0700

    added mobile-friendly navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,40 @@
+import type React from "react"
 import "./styles/Navbar.css"
+import "../../../static/images/Hamburger_icon.svg"
+import { useEffect } from "react"
 
 type NavbarProps = {
 	isLoggedIn: boolean
 	username: string | undefined
 }
+
 const Navbar: React.FC<NavbarProps> = ({ isLoggedIn, username }) => {
+
+
+
+	function toggleMobileNavVisibiity(event: React.MouseEvent<HTMLDivElement>) {
+		event.preventDefault()
+
+		const mobileNav = document.getElementById("mobileNav")!
+		const toggleIcon = document.getElementById("hamburgerIcon")!
+
+		if (mobileNav.style.display === "none") {
+			mobileNav.style.display = ""
+			toggleIcon.innerText = "🗙"
+		} else {
+			mobileNav.style.display = "none"
+			toggleIcon.innerText = "☰"
+		}
+	}
+
+	useEffect(() => {
+		const mobileNav = document.getElementById("mobileNav")
+
+		if (mobileNav) {
+			mobileNav.style.display = "none"
+		}
+	}, [])
+
 	return (
 		<>
 			<nav className="navbar">
@@ -12,23 +42,28 @@ const Navbar: React.FC<NavbarProps> = ({ isLoggedIn, username }) => {
 					Rolladex
 				</a>
 
-				{isLoggedIn ?
-					<ul className="links">
-						<li>
+				{isLoggedIn &&
+
+					<>
+						<div id="burgerAndMobileNav">
+							<div id="hamburgerIcon" onClick={toggleMobileNavVisibiity}>☰</div>
+							<nav id="mobileNav">
+								<a href="/home">
+									Home
+								</a>
+								<a href="/friends?sortby=default&page=1&perpage=default">
+									Network
+								</a>
+								<a href="/logout">Logout</a>
+							</nav>
+						</div>
+						<nav id="desktopNav">
 							<a href="/friends?sortby=default&page=1&perpage=default">
 								Network
 							</a>
-						</li>
-						{username && (
-							<li>
-								<a href="/profile">{username}</a>
-							</li>
-						)}
-						<li>
 							<a href="/logout">Logout</a>
-						</li>
-					</ul>
-					: <></>}
+						</nav>
+					</>}
 			</nav>
 		</>
 	)
